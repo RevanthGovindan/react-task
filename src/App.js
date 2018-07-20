8562c15513dde01fdd427550096639bfd8565d79
RevanthGovindan <revanthji14@gmail.com>
2018-07-20 18:06:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <button className="btn btn-primary">Add New</button>
         <div className="checklist">
-          <List/>
+          <List />
         </div>
       </div>
     );
@@ -16,37 +16,67 @@ class App extends Component {
 class List extends Component {
   constructor(props) {
     super(props);
-    this.state = { data:[{ name: "qwe", age: 10 }, { name: "rev", age: 21 }, { name: "saran", age: 23 }]};
+    this.state = { data: [{ title: "title1", items: ['abc', 'bcd', 'cde'] }, { title: "title2", items: ['abc', 'bcd', 'cde'] }, { title: "title3", items: ['abc', 'bcd', 'cde'] }] }
     this.show = this.show.bind(this);
-   
+    this.handleChange=this.handleChange.bind(this);
   }
-  handleChange(i,event){
-    const items = this.state.data;
-    const n=event.target.name;    
-    if(n==="name1")
-    {
-      items[i].name=event.target.value;
+  handleChange(i, event) {
+    const data = this.state.data;
+    const name = event.target.name;
+    if (name === "title") {
+      data[i].title = event.target.value;
     }
-    else if(n==="age"){
-      items[i].age=event.target.value;
-    }
-    this.setState({items})
+    this.setState({ data })
     event.preventDefault();
   }
-  show(){
+  show() {
     console.log(this.state.data);
-    }
+  }
+  removeTask(titleIndex, taskIndex) {
+    const data = this.state.data[titleIndex].items;
+    data.splice(taskIndex, 1);
+    this.setState({ data })
+  }
+  addTask(titleIndex){
+    const data = this.state.data;
+    data[titleIndex].items.push('new task');
+    this.setState({data});
+  }
   render() {
     return (
       <div className="box">
-      {
-
-        this.state.data.map((dat,i)=><div className="rounded item">
-                                        <input value={dat.name} name="name1" onChange={this.handleChange.bind(this,i)} id="inpt"/>
-                              <br></br><input value={dat.age} name="age" onChange={this.handleChange.bind(this,i)}  id="inpt"/></div>)
-                              
-      }
-      <button onClick={this.show}>click</button>
+        {
+          this.state.data.map(function (data, i) {
+            return (<div><div>
+              <input value={data.title} name="title"  id="inpt" />
+              {
+                data.items.map(function (item, j) {
+                  return (<h4><input type="checkbox" value={item} />{item}<button type="button" className="close" aria-label="Close" >
+                    <span aria-hidden="true">&times;</span>
+                  </button></h4>)
+                })
+              }
+              <button onClick={this.addTask.bind(this,i)}>Add</button>
+              </div>
+            </div>)
+          })
+        }
+        <button onClick={this.show}>click</button>
+        {/* {
+          this.state.data.map((dat, i) => 
+            <div>
+              <div className="datas">
+                <input value={dat.title} name="title" onChange={this.handleChange.bind(this, i)} id="inpt" />
+                {dat.items.map((itm,j) => itm!==undefined &&<h4>
+                  <input type="checkbox" value={itm} />{itm}<button type="button" className="close" aria-label="Close" onClick={this.removeTask.bind(this,i,j)}>
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </h4>)}
+              </div>
+            </div>
+          )
+        }
+        <button onClick={this.show}>click</button> */}
       </div>
     );
   }
