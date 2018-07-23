dd6c8041830ae1bff67eab63339eb5152c9f5864
RevanthGovindan <revanthji14@gmail.com>
2018-07-23 12:58:36 +0530

    changes for list

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button className="btn btn-primary">Add New</button>
+
         <div className="checklist">
           <List />
         </div>
@@ -17,66 +17,66 @@ class List extends Component {
   constructor(props) {
     super(props);
     this.state = { data: [{ title: "title1", items: ['abc', 'bcd', 'cde'] }, { title: "title2", items: ['abc', 'bcd', 'cde'] }, { title: "title3", items: ['abc', 'bcd', 'cde'] }] }
-    this.show = this.show.bind(this);
-    this.handleChange=this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  handleChange(i, event) {
+  handleChange(titleIndex, event) {
     const data = this.state.data;
     const name = event.target.name;
     if (name === "title") {
-      data[i].title = event.target.value;
+      data[titleIndex].title = event.target.value;
     }
     this.setState({ data })
     event.preventDefault();
   }
-  show() {
-    console.log(this.state.data);
-  }
   removeTask(titleIndex, taskIndex) {
-    const data = this.state.data[titleIndex].items;
-    data.splice(taskIndex, 1);
-    this.setState({ data })
+    const data1 = this.state.data[titleIndex].items;
+    data1.splice(taskIndex, 1);
+    this.setState({ data1 })
+    console.log(data1.length)
   }
-  addTask(titleIndex){
+  addTask(titleIndex) {
     const data = this.state.data;
-    data[titleIndex].items.push('new task');
-    this.setState({data});
+    const number = data[titleIndex].items.length + 1;
+    data[titleIndex].items.push('new' + number);
+    this.setState({ data });
+  }
+  addTitle() {
+    const data = this.state.data;
+    const body = { title: 'title', items: ['new'] }
+    data.push(body);
+    this.setState({ data });
+  }
+  removeTitle(titleIndex) {
+    const data1 = this.state.data;
+    data1.splice(titleIndex, 1);
+    this.setState({ data1 });
   }
   render() {
     return (
-      <div className="box">
-        {
-          this.state.data.map(function (data, i) {
-            return (<div><div>
-              <input value={data.title} name="title"  id="inpt" />
-              {
-                data.items.map(function (item, j) {
-                  return (<h4><input type="checkbox" value={item} />{item}<button type="button" className="close" aria-label="Close" >
-                    <span aria-hidden="true">&times;</span>
-                  </button></h4>)
-                })
-              }
-              <button onClick={this.addTask.bind(this,i)}>Add</button>
+      <div>
+        <button className="btn btn-primary" onClick={this.addTitle.bind(this)}>Add New</button>
+        <div className="box">
+          {
+            this.state.data.map((data, titleIndex) =>
+              <div>
+                <div className="datas">
+                  <div>
+                    <input value={data.title} name="title" onChange={this.handleChange.bind(this, titleIndex)} id="inpt" />
+                    <button type="button" className="close" aria-label="Close" onClick={this.removeTitle.bind(this, titleIndex)}>
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  {data.items.map((item, taskIndex) => <h4 key={item}>
+                    <input type="checkbox" value={item} />{item}<button type="button" className="close" aria-label="Close" onClick={this.removeTask.bind(this, titleIndex, taskIndex)}>
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </h4>)}
+                  <button onClick={this.addTask.bind(this, titleIndex)}>Add</button>
+                </div>
               </div>
-            </div>)
-          })
-        }
-        <button onClick={this.show}>click</button>
-        {/* {
-          this.state.data.map((dat, i) => 
-            <div>
-              <div className="datas">
-                <input value={dat.title} name="title" onChange={this.handleChange.bind(this, i)} id="inpt" />
-                {dat.items.map((itm,j) => itm!==undefined &&<h4>
-                  <input type="checkbox" value={itm} />{itm}<button type="button" className="close" aria-label="Close" onClick={this.removeTask.bind(this,i,j)}>
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </h4>)}
-              </div>
-            </div>
-          )
-        }
-        <button onClick={this.show}>click</button> */}
+            )
+          }
+        </div>
       </div>
     );
   }
