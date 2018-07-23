22dba39cce57b9e4ce1e483b3723b04e0dcd330d
RevanthGovindan <revanthji14@gmail.com>
2018-07-23 14:44:28 +0530

    23

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
         <div className="checklist">
           <List />
         </div>
@@ -32,7 +31,6 @@ class List extends Component {
     const data1 = this.state.data[titleIndex].items;
     data1.splice(taskIndex, 1);
     this.setState({ data1 })
-    console.log(data1.length)
   }
   addTask(titleIndex) {
     const data = this.state.data;
@@ -58,20 +56,20 @@ class List extends Component {
         <div className="box">
           {
             this.state.data.map((data, titleIndex) =>
-              <div>
+              <div className="item"> 
                 <div className="datas">
-                  <div>
+                  <div className="titlebar">
                     <input value={data.title} name="title" onChange={this.handleChange.bind(this, titleIndex)} id="inpt" />
-                    <button type="button" className="close" aria-label="Close" onClick={this.removeTitle.bind(this, titleIndex)}>
+                    <button type="button" className="close cls" aria-label="Close" onClick={this.removeTitle.bind(this, titleIndex)}>
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  {data.items.map((item, taskIndex) => <h4 key={item}>
+                  {data.items.map((item, taskIndex) => <li key={item}>
                     <input type="checkbox" value={item} />{item}<button type="button" className="close" aria-label="Close" onClick={this.removeTask.bind(this, titleIndex, taskIndex)}>
                       <span aria-hidden="true">&times;</span>
                     </button>
-                  </h4>)}
-                  <button onClick={this.addTask.bind(this, titleIndex)}>Add</button>
+                  </li>)}
+                  <button className="btn btn-danger" onClick={this.addTask.bind(this, titleIndex)}>Add</button>
                 </div>
               </div>
             )
